eda7465e3c2acd2a9d7107843a719f1da9996df3
Alphonse Bendt <370821+abendt@users.noreply.github.com>
2024-05-07 21:30:25 +0200

    Update dependencies (#4)

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,19 +1,23 @@
 plugins {
     `java-gradle-plugin`
     id("maven-publish")
-    id("com.gradle.plugin-publish") version "0.18.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
 
     // this should be set to 1.5.31 as this is the bundled version of gradle 7.3
-    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    id("org.jetbrains.kotlin.jvm") version "1.9.23"
 }
 
 group = "io.github.redgreencoding"
-version = "0.0.2"
+version = project.findProperty("projectVersion") ?: "0.0.3-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 repositories {
@@ -23,26 +27,28 @@ repositories {
 dependencies {
     implementation(gradleApi())
 
-    implementation("org.barfuin.texttree:text-tree:2.1.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    val koTestVersion = "4.6.3"
+    implementation("org.barfuin.texttree:text-tree:2.1.2")
+
+    val koTestVersion = "5.8.1"
     testImplementation("io.kotest:kotest-runner-junit5:$koTestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$koTestVersion")
 }
 
 gradlePlugin {
-    val findClass by plugins.creating {
-        id = "io.github.redgreencoding.findclass"
-        displayName = "FindClass Gradle Plugin"
-        description = "A plugin that helps you find out from which .jar archive a class is loaded from"
-        implementationClass = "io.github.redgreencoding.findclass.FindClassGradlePlugin"
-    }
-}
+    website.set("https://github.com/red-green-coding/findclass-gradle-plugin")
+    vcsUrl.set("https://github.com/red-green-coding/findclass-gradle-plugin.git")
 
-pluginBundle {
-    website = "https://github.com/red-green-coding/findclass-gradle-plugin"
-    vcsUrl = "https://github.com/red-green-coding/findclass-gradle-plugin.git"
-    tags = listOf("findclass", "searchclass", "dependencies", "analyze", "classpath")
+    plugins {
+        create("findClass") {
+            id = "io.github.redgreencoding.findclass"
+            displayName = "FindClass Gradle Plugin"
+            description = "A plugin that helps you find out from which .jar archive a class is loaded from"
+            implementationClass = "io.github.redgreencoding.findclass.FindClassGradlePlugin"
+            tags.set(listOf("findclass", "searchclass", "dependencies", "analyze", "classpath"))
+        }
+    }
 }
 
 publishing {
@@ -83,10 +89,6 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         allWarningsAsErrors = true
 
-        jvmTarget = "1.8"
-        apiVersion = "1.5"
-        languageVersion = "1.5"
-
         incremental = true
         freeCompilerArgs = listOf(
             "-Xjsr305=strict"
@@ -99,6 +101,7 @@ tasks {
         enableStricterValidation.set(true)
         failOnWarning.set(true)
     }
+
     jar {
         from(sourceSets.main.map { it.allSource })
         manifest.attributes.apply {
